dcf2ef1882d1fdb9b8b3e85e3436b9069bd7bb6f
Tronx Group <tronxgroupspa@gmail.com>
2026-04-11 16:18:35 -0400

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -111,8 +111,8 @@ export function Navbar() {
           <Image
             src="/logo_text_white.png"
             alt="Dekaelo Media"
-            width={160}
-            height={40}
+            width={102}
+            height={98}
             className="object-contain"
             priority
           />
