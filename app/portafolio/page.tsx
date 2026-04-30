a01f09b1a8d3180e181851378eff36a4f869917f
Tronx Group <tronxgroupspa@gmail.com>
2026-04-30 17:46:04 -0400

    Update page.tsx

--- app/portafolio/page.tsx
+++ app/portafolio/page.tsx
@@ -49,7 +49,7 @@ const categories: Category[] = [
     label: "Casos seleccionados",
     items: [
       {
-        client: "BiceCorp",
+        client: "BICECORP",
         name: "Nos Une",
         year: "2024 - 2026",
         tag: "Vodcast interno",
